d3b5c3eedbe85daf74b6d299d3acf5e4d50f76bc
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-05-27 21:42:19 +0300

    seeding

--- lib/seed.py
+++ lib/seed.py
@@ -9,6 +9,42 @@ def seed_database():
     session.query(User).delete()
     session.query(Item).delete()
     session.query(Order).delete()
-    session.query(Order_Items).delete()
+    session.query(OrderItems).delete()
+    
+    
+    
+    users_data = []
+    
+    users = []
+    
+    for user_data in users_data:
+        user = User(**user_data)
+        users.append(user)
+    session.add_all(users)
+    
+    
+    items_data = []
+    
+    items = []
+    for item_data in items_data:
+        item = Item(**item_data)
+        items.append(item)
+        session.add_all(items)
+        
+    session.commit()
+    
+        
+        #sample orders to check if code is functional
+        
+        
+        
+    session.commit
+    session.close()
+    print("Database seeded successfully.")
+    
+if __name__ == "__main__":
+    seed_database()    
+        
+        
 
     
